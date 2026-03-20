e0596ff808d494121c098b0e3e6122f087951d0b
Matheus797-pgn <matheushenriqueramiro12345@gmail.com>
2026-03-20 20:11:08 -0300

    atividade 2

---
+++ POO/Aula-02/atividade 2/atividade 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using atividade_2;
+using static System.Console;
+try
+{
+    Salario s = new Salario();
+    WriteLine("Digite o nome do usuario");
+    s.nome = Console.ReadLine();
+    WriteLine("Digite o salario do funcionario: ");
+    s.SalarioBruto = double.Parse(ReadLine());
+    WriteLine("Digite o valor do imposto ao salario");
+    s.imposto = double.Parse(ReadLine());
+    s.Dadosdoimposto();
+    s.Resultadodosalario();
+    WriteLine("Digite a porcentagem  do aumento do salario");
+    s.AumentoporPorcentagem = double.Parse(ReadLine());
+    s.Resultado();
+    s.Saida();
+    ReadKey();
+}
+catch (Exception)
+{
+    Write("DIGITE SOMENTE O NECESSARIO QUE ESTA PEDINDO ENTENDEU? OU QUER QUE EU DESENHE?");
+}
+
---
+++ POO/Aula-02/atividade 2/atividade 2/Salario.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace atividade_2
+{
+    internal class Salario
+    {
+        public string nome;
+        public double SalarioBruto;
+        public double SalarioLiquido;
+        public double AumentoporPorcentagem;
+        public double dados;
+        public double imposto;
+
+        public double Dadosdoimposto()
+        {
+            return SalarioLiquido = SalarioBruto - imposto;
+
+        }
+        public void Resultadodosalario()
+        {
+           Console.WriteLine($"Dados do funcionario");
+           Console.WriteLine($" nome do funcionario {nome}");
+           Console.WriteLine($"Salario liquido {SalarioLiquido}");
+        }
+
+        public double Resultado()
+        {
+            AumentoporPorcentagem = AumentoporPorcentagem * 0.01;
+            return dados = SalarioLiquido * AumentoporPorcentagem;
+            
+
+        }
+        public void Saida()
+        {
+            Console.WriteLine($"Dados atualizados");
+            Console.WriteLine($"nome do funcionario {nome}");
+            Console.WriteLine($"salario atualizado {SalarioLiquido + dados}");
+        }
+    }
+}
