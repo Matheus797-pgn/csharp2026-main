d91d6515b578805656d710cbf0f704491c67d209
Matheus797-pgn <matheushenriqueramiro12345@gmail.com>
2026-03-23 21:56:33 -0300

    Aula-04

---
+++ POO/Aula-02/circuferencia01/circuferencia01/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using static System.ConsoleColor;
+Title = "calculadora 01";
+double raio, circunferencia, volume;
+const double PI = 3.14;
+
+// entrada de dados
+Write("entre com o valor do raio da circuferencia");
+raio = double.Parse(ReadLine());
+ResetColor();
+
+//saida de dados
+WriteLine($"Circuferencia: {Circuferencia(raio):.F2}");
+WriteLine($"Volume: {Volume(raio):.F2}");
+WriteLine($"PI: {PI}");
+// calculo da circuferencia
+double Circuferencia(double r)
+{
+    return 2 * PI * r;
+}
+double Volume(double r)
+{
+    return (4 / 3) * PI * r;
+}
+
