185c84b5e5b1ffc057276ceef9a72fd05bacdb1c
Matheus797-pgn <matheushenriqueramiro12345@gmail.com>
2026-03-25 21:51:31 -0300

    Aula-05

---
+++ POO/Aula-02/banco/banco/Conta.cs
@@ -0,0 +1,39 @@
+﻿
+namespace banco
+{
+    internal class Conta
+    {
+        private int numero;
+        private string titular;
+        private double saldo;
+
+
+        public Conta (int numero, string titular)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            saldo = 0;
+
+        }
+        public Conta(int numero,string titular , double saldo): this(numero, titular)
+        {
+            this.saldo = saldo;
+        }
+        
+        //metodos
+        public void Deposito(double quantia)
+        {
+            saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            saldo -= quantia;
+        }
+        public void Dados()
+        {
+            Console.WriteLine("dados da conta cadastrada");
+            Console.WriteLine($"\tTitular: {titular}\n" + $"\tNumero da conta: {numero}\n" + $"\tsaldo da conta: ${saldo}");
+        }
+    }
+}
---
+++ POO/Aula-02/banco/banco/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using banco;
+using static System.Console;
+try
+{
+    Title = "abertura de conta";
+    Conta conta;
+
+    WriteLine("Abertura de conta - Nika investment");
+    Write("entre com um numero de conta");
+    int numero = int.Parse(ReadLine());
+    Write("Entre com o nome do titular ");
+    string nome = ReadLine();
+    Write("deseja fazer deposito (s/n)");
+    char resposta = char.Parse(ReadLine().ToLower());
+
+    if (resposta == 's')
+    {
+        Write("Entre com valor de deposito inicial");
+        double deposito = double.Parse(ReadLine());
+        conta = new Conta(numero, nome, deposito);
+    }
+    else
+    {
+        conta = new Conta(numero, nome);
+    }
+    conta.Dados();
+    WriteLine("Entre com um valor para deposito");
+    double quantia = double.Parse(ReadLine());
+    conta.Deposito(quantia);
+    WriteLine("Entr com um valor para saque R$");
+    conta.Saque(quantia);
+    ReadKey();
+}
+catch (Exception ex)
+{
+    WriteLine($"DIGITE SOMENTE O NECESSARIO MULA {ex.Message}");
+}
