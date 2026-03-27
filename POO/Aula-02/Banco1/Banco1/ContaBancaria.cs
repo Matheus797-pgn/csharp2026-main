5ce5723f3be851e97dba0d4859a1240b1db75370
Matheus797-pgn <matheushenriqueramiro12345@gmail.com>
2026-03-27 20:56:09 -0300

    Aula-06

---
+++ POO/Aula-02/Banco1/Banco1/ContaBancaria.cs
@@ -0,0 +1,67 @@
+﻿
+
+namespace Banco1
+{
+    internal class ContaBancaria
+    {
+        // campos
+        private int numero;
+        private string titular;
+        private double saldo;
+       
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+
+        }
+         public string Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+
+        }
+        public double Saldo
+        {
+            get { return saldo; }
+            set
+            {
+                if (value > 0)
+                {
+                    saldo = value;
+                }
+                else
+                {
+                    saldo = 0;
+                }
+
+            }
+        }
+       
+             //construtor
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            saldo = 0;
+        }
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+        public void deposito(double quantia)
+        {
+            saldo += quantia;
+        }
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+        }
+        public void Saida()
+        {
+            Console.WriteLine($" o numero da conta é {numero}");
+            Console.WriteLine($"O nome do usuario é {Titular}");
+            Console.WriteLine($"O saldo da sua conta é {saldo:C}");
+        }
+    }
+}
---
+++ POO/Aula-02/Banco1/Banco1/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using  Banco1;
+Title = "cadastro de conta";
+ContaBancaria conta;
+try
+{
+    //entrada de dados
+    Write("entre com o numero da conta: ");
+    int n = int.Parse(ReadLine());
+    Write("entre com o nome do titular da conta");
+    string nome = ReadLine();
+    Write("deseja fazer 1 deposito inicial? (s/n)");
+    char resposta = char.Parse(ReadLine().ToLower());
+    if (resposta == 's')
+    {
+        Write("entre om o valor de deposito inicial");
+        double dep = double.Parse(ReadLine());
+        conta = new ContaBancaria(n, nome, dep);
+
+    }
+    else
+    {
+        conta = new(n, nome);
+    }
+    conta.Saida();
+    ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ESCREVA SÓ O DESEJADO{ex.Message} ");
+}
